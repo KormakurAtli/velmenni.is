6c0bc065009d268e797332a2a661dd6a5356fbb3
Kormákur Atli <katli1604@gmail.com>
2019-02-28 19:59:02 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Vélmenni.is</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<div class="header">
+			<hr>
+			<div class="icon">
+				<a href="#"><img class="img" src="img/icon.png"></a>
+			</div>
+			<div class="nav">
+				<a href="">FRÉTTIR</a><!--
+				--><a href="">STYRKTARAÐILAR</a><!--
+				--><a href="">MYNDIR</a>
+			</div>
+			<hr>
+		</div>
+	</header>
+	<div class="front_img">
+		<img src="img/front_image_5.jpg">
+		<div class="centered">
+			<h1>LANDSLIÐIÐ Í VÉLMENNAFORRITUN</h1>
+		</div>
+	</div>
+	<div class="mid">
+		
+		<h1>LANGAR ÞIG AÐ VERA Í LANDSLIÐINU Í VÉLMENNAFORRITUN?</h1>
+		<a class="button_link" href="#">
+			<div class="button">
+				<p>SÆKTU UM!</p>
+			</div>
+		</a>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+</body>
+</html>
